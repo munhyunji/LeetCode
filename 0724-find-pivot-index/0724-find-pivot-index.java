34a341c5d64bc097c73530b7ae99b8f998c74d55
Munhyunji <84836128+munhyunji@users.noreply.github.com>
2024-06-12 17:46:09 +0900

    Time: 1 ms (94.82%), Space: 45.3 MB (59.71%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.java
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -9,11 +9,18 @@ class Solution {
          int leftSum = 0;
          int rightSum = 0;
          for(int i = 0; i < nums.length; i++) {
-             rightSum = totalSum - leftSum - nums[i];  
+             rightSum = totalSum - leftSum - nums[i];
+             //,7,3,6,5,6
+             //,3,6,5,6
+             //,6,5,6
+             //,5,6
              if(leftSum == rightSum) {                
                 return i;                
              }
-             leftSum += nums[i];     
+             leftSum += nums[i];   
+             //1,
+             //1,7
+             //1,7,3
                                                                            
          }
          return -1;
