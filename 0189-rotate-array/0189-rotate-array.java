257e677ae5e403dc6d0bc5f4926cf78cbc300663
Munhyunji <84836128+munhyunji@users.noreply.github.com>
2024-06-18 11:26:52 +0900

    Time: 0 ms (100.00%), Space: 57.4 MB (40.05%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -1,6 +1,7 @@
 class Solution {
     public void rotate(int[] nums, int k) {
-         k %= nums.length;
+         k %= nums.length; // k가 배열의 길이보다 큰 경우를 처리하기 위해 k를 조정
+        //k가 7이고 num.length가 5인경우 7 % 5 로 2가된다. 그러므로 배열이 두번 회전한 결과가 됨 
         int n = nums.length-1;
         reverse(nums, 0, n);
         reverse(nums, k, n);
