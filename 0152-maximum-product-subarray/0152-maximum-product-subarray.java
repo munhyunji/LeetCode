2af5d7e771ab5632675eefb9b90848eba820ec05
Munhyunji <84836128+munhyunji@users.noreply.github.com>
2024-06-18 17:08:27 +0900

    Time: 2 ms (60.15%), Space: 44.9 MB (32.05%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -4,20 +4,19 @@ class Solution {
         int curMin = nums[0];
         int curMax = nums[0];
         int totalMax = nums[0]; //6
-        //[-2,0,-1] = 0
-        //  i
+        //[2,3,-2,4]
+        //        i
         for(int i = 1; i < nums.length; i++) {
                 
-            if(nums[i] < 0) {
+            if(nums[i] < 0) { //음수곱인경우를 생각해서 max값과 min값을 바꿔준다
                 int tmp = curMin;
                 curMin = curMax;
                 curMax = tmp;
             }
-             curMin = Math.min(nums[i], nums[i] * curMin); //-6
-             curMax = Math.max(nums[i], nums[i] * curMax); //6
-            
-  
-              totalMax = Math.max(totalMax, curMax);
+             curMin = Math.min(nums[i], nums[i] * curMin); //  -12
+             curMax = Math.max(nums[i], nums[i] * curMax); // -2
+              
+             totalMax = Math.max(totalMax, curMax);
             
         }
        return totalMax; 
