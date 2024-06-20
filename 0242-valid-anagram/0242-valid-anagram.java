227e85d021b032b75ab13c23b91a0394e8928cf1
Munhyunji <84836128+munhyunji@users.noreply.github.com>
2024-06-20 12:04:18 +0900

    Time: 4 ms (71.21%), Space: 44.4 MB (39.08%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -10,3 +10,20 @@ class Solution {
         return Arrays.equals(strs,strt);
     }
 }
+
+// HashMap version .....
+//       HashMap<String, Boolean> hsm = new HashMap<>();
+        
+//              char[] cs = s.toCharArray();
+//             Arrays.sort(cs);
+//             String sortedString1 = new String(cs); //anagram
+//             hsm.put(sortedString1, true);
+
+//             char[] ts = t.toCharArray();
+//             Arrays.sort(ts);
+//             String sortedString2 = new String(ts);
+            
+//             if(hsm.containsKey(sortedString2)) {
+//                 return true;
+//             }
+//         return false;
