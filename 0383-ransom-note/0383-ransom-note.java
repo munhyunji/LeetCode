7e673c0d3e7dd74107396b5e727b23244a7d3b12
Munhyunji <84836128+munhyunji@users.noreply.github.com>
2024-07-09 16:07:50 +0900

    Time: 14 ms (30.61%), Space: 45.3 MB (7.74%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -7,12 +7,12 @@ class Solution {
             hsm.put(c, hsm.getOrDefault(c, 0) +1);
         }
         
-        for( char c : ransomNote.toCharArray()) { // (a, 2) (b, 1)
-            if(hsm.containsKey(c) && hsm.get(c) > 0) {
-                   hsm.put(c, hsm.getOrDefault(c, 0) -1);
-               } else {
-                   return false;
-               }            
+        for ( char c : ransomNote.toCharArray()) {
+           if (hsm.containsKey(c) && hsm.get(c) > 0) { //문자가 하나라도 -1 이돤다면 
+               hsm.put(c, hsm.getOrDefault(c, 0)-1);
+           } else {
+               return false;
+           }
         } 
         return true;
     }
