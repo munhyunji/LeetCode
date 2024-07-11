79e2a6ef3a4e64ea8d95eb652f3223e8ef19b075
Munhyunji <84836128+munhyunji@users.noreply.github.com>
2024-07-11 12:15:35 +0900

    Time: 0 ms (100.00%), Space: 41.3 MB (20.55%) - LeetHub

--- 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.java
+++ 0092-reverse-linked-list-ii/0092-reverse-linked-list-ii.java
@@ -10,32 +10,33 @@
  */
 class Solution {
     public ListNode reverseBetween(ListNode head, int left, int right) {
+        
         ListNode dummy = new ListNode(0);
         dummy.next = head;
-
+        
         ListNode leftPrevNode = dummy;
         ListNode leftNode = head;
-
-        //1) left point까지 움직인다 이전 포인터와 함께
+        
+        //1) move leftNode
         for(int i = 0; i < left-1; i++) {
-         leftPrevNode = leftNode; 
-         leftNode = leftNode.next;   
+            leftPrevNode = leftNode;
+            leftNode = leftNode.next;
         }
         
-        //2) reverse한다         
-        ListNode tmp = null;
+        //2) turn arrows
         ListNode prev = null;
-        for(int i = 0; i < right-left+1; i++) { //gap+1
-            tmp = leftNode.next; //tmp가 다음 node를 가르키게 함 
+        ListNode tmp = null;
+        for(int i = 0; i < right-left+1; i++) {
+            tmp = leftNode.next;
             leftNode.next = prev;
-            prev = leftNode; 
-            leftNode = tmp;                        
+            prev = leftNode;
+            leftNode = tmp;
         }
         
-        //3) 끊어진 노드들을 연결  .. 
+        //3) 끝과 끝을 연결 
         leftPrevNode.next.next = leftNode;
         leftPrevNode.next = prev;
         
-        return dummy.next; 
+        return dummy.next;
     }
 }
