17ff830eb6caacb76423f19cb3e1358fd92e6ed7
Munhyunji <84836128+munhyunji@users.noreply.github.com>
2024-07-15 12:22:46 +0900

    Time: 0 ms (100.00%), Space: 41.6 MB (42.10%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -19,10 +19,9 @@ class Solution {
     }
     
     public boolean isMirror(TreeNode t1, TreeNode t2) {
-        if(t1 == null && t2 == null) return true; //both empty
-        if(t1 == null || t2 == null) return false; //either null 
-
-        return t1.val == t2.val && isMirror(t1.left, t2.right) && isMirror(t1.right, t2.left);
-        //하나라도 false라면 mirror가 아님  
+        if(t1 == null && t2 == null) return true;
+        if(t1 == null || t2 == null || t1.val != t2.val) return false;
+        
+        return isMirror(t1.left, t2.right) && isMirror(t1.right, t2.left);
     }
 } 
