715200dd6d1aec925f38b929e303d77800b14d58
Munhyunji <84836128+munhyunji@users.noreply.github.com>
2024-07-15 13:58:51 +0900

    Time: 2 ms (96.64%), Space: 45.6 MB (48.67%) - LeetHub

--- 0637-average-of-levels-in-binary-tree/0637-average-of-levels-in-binary-tree.java
+++ 0637-average-of-levels-in-binary-tree/0637-average-of-levels-in-binary-tree.java
@@ -15,30 +15,25 @@
  */
 class Solution {
     public List<Double> averageOfLevels(TreeNode root) {
-        
-      List<Double> average = new ArrayList<>();
-      Queue<TreeNode> queue = new LinkedList<>();
+        List<Double> average = new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
         
         queue.add(root);
         while(!queue.isEmpty()) {
-            double size = queue.size(); //size를 먼저 설정
-            double averageOfLevels = 0.0;
+            double size = queue.size();
+            double averOfNodes = 0.0;
             
             for(int i = 0; i < size; i++) {
-                TreeNode currentNode = queue.poll(); //for문 안쪾에 설정해줘야 모든 노드확인
-                averageOfLevels += currentNode.val;
+                TreeNode cur = queue.poll();
+                averOfNodes += cur.val;
                 
-                if(currentNode.left != null) {
-                    queue.add(currentNode.left);
-                }
-                
-                if(currentNode.right != null) {
-                    queue.add(currentNode.right);
-                }
+                if(cur.left != null) queue.add(cur.left);
+                if(cur.right != null) queue.add(cur.right);
             }
-            averageOfLevels = averageOfLevels / size; //평균이니 size대로 나누고 double로 설정
-            average.add(averageOfLevels);
+            averOfNodes = averOfNodes / size;
+            average.add(averOfNodes);
+            
         }
-       return average; 
+        return average;
     }
 }
