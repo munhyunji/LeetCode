71f5127819b2dac5c897de313e7a3e45fd111cb7
Munhyunji <84836128+munhyunji@users.noreply.github.com>
2024-07-16 11:15:48 +0900

    Time: 0 ms (100.00%), Space: 44.8 MB (11.59%) - LeetHub

--- 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.java
+++ 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.java
@@ -15,22 +15,20 @@
  */
 class Solution {
     public int getMinimumDifference(TreeNode root) {
-        inOrder(root); //따로 메소드 생성해줘야 초기화되지않음 
-        return minValue; 
+        inOrder(root);
+        return min;
     }
+    int min = Integer.MAX_VALUE;
+    Integer prev = null;
     
-    private int minValue = Integer.MAX_VALUE; //비교할 값
-    private Integer prev = null; //이전 노드값을 저장할 Integer 변수 (null도 허용함)
-    
-    public void inOrder(TreeNode root) {
-        if(root == null) return;
+    public void inOrder(TreeNode root){
+        if(root==null) return;
         
-        inOrder(root.left); //InOrder 함수 호출후 
-        if(prev != null) {
-            minValue = Math.min(minValue, Math.abs(root.val - prev)); // 중간에서 값구하기 
+        inOrder(root.left);
+        if(prev != null){
+            min = Math.min(min, root.val-prev);
         }
         prev = root.val;
         inOrder(root.right);
-        
     }
 }
