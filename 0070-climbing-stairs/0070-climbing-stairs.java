c2981515dad4a83c1de64e726f5391c626cb2df5
Munhyunji <84836128+munhyunji@users.noreply.github.com>
2024-08-12 15:13:39 +0900

    Time: 0 ms (100.00%), Space: 40.2 MB (61.87%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,20 +1,15 @@
 class Solution {
     public int climbStairs(int n) {
         
-        int[] array = new int[n+1];
+        int[] dp = new int[n+1];
+        int one = dp.length-1;
+        int two = dp.length-2;
+        dp[one] = 1;
+        dp[two] = 1;
         
-        int onePoint = array.length-2;
-        int twoPoint = array.length-1;
-        //[   1  1 ]
-        // 0, 1, 2
-        //    o  t
-        array[onePoint] = 1;
-        array[twoPoint] = 1;
-        
-        for(int i = array.length-3; i >=0; i-- ) {
-            array[i] = array[onePoint--] + array[twoPoint--];
+        for(int i=dp.length-3; i>=0; i--) {
+            dp[i] = dp[one--] + dp[two--]; 
         }
-        return array[0];
+        return dp[0];
     }
-    
 }
