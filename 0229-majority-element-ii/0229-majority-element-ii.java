ce0b1b3a6d2de7b9f0d1f6d722e1f17551189374
Munhyunji <84836128+munhyunji@users.noreply.github.com>
2024-08-18 10:55:39 +0900

    Time: 11 ms (42.02%), Space: 47.1 MB (95.98%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,7 +1,7 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
         
-        HashMap<Integer, Integer> hsm = new HashMap<>();
+        Map<Integer, Integer> hsm = new HashMap<>();
         List<Integer> numList = new ArrayList<>();
         
         for(int num : nums) {
