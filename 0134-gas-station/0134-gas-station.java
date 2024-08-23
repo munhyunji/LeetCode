ac5065a759d535ea4387e3672406298dc177a1ea
Munhyunji <84836128+munhyunji@users.noreply.github.com>
2024-08-23 15:31:17 +0900

    Time: 2 ms (99.33%), Space: 56 MB (94.79%) - LeetHub

--- 0134-gas-station/0134-gas-station.java
+++ 0134-gas-station/0134-gas-station.java
@@ -3,22 +3,22 @@ class Solution {
         int gasTotal = 0;
         int costTotal = 0;
         
-        for(int g : gas) gasTotal += g; 
+        for(int g : gas) gasTotal += g;
         for(int c : cost) costTotal += c;
         
-        if(gasTotal < costTotal) return -1;
+        if( gasTotal < costTotal ) return -1;
         
         int total = 0;
         int index = 0;
-        
-        for(int i = 0; i <gas.length; i++) {
-            total += gas[i] - cost[i];
-            
-            if(total < 0) {
-              total = 0;  
-                index = i + 1;           
-            } 
-        }
-        return index; 
+            for(int i = 0; i < gas.length; i++) {
+                total += gas[i] - cost[i];
+
+                if(total < 0) {
+                    total = 0;
+                    index = i+1;
+                }
+            }
+        return index;
+
     }
 }
